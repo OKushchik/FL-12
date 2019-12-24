7c8dbbe1d31079879f1eee67038b421f48219e02
Oksana Kushchik <okushchik@gmail.com>
2019-12-24 21:40:28 +0200

    Add files via upload

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,18 +2,18 @@ let a = prompt('Введите число А');
 let b = prompt('Введите число B');
 let c = prompt('Введите число C');
  if (isNaN(+a) === true || Number.isNaN(+b) === true || Number.isNaN(+c) === true) {
-     alert('Enter a number please');
+     console.log('Enter a number please');
  } else if ( a === '0'){
-     alert(' A cant be 0')
+     console.log('A cant be 0');
  } else {
     let d = b*b-4*a*c;
      if (d<0){
-         alert('net kornej')
+         console.log('net kornej');
      } else if (b === '0' && c === '0') {
-         alert('x=0')
+         console.log('x=0');
      } else if ( d>0) {
          let x1 = (-b + Math.sqrt(d))/2*a;
          let x2 = (-b - Math.sqrt(d))/2*a;
-         alert('X1='+x1+' X2='+x2 )
+         console.log('X1='+x1+' X2='+x2) ;
      }
  }
